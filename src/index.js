0191cec864e1e4585b570bb8af1055839a84134d
david <davidw.rq@gmail.com>
2017-11-19 18:03:41 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const request = require('request')
+const cheerio = require('cheerio')
+const url = 'http://www.gocomics.com/calvinandhobbesespanol'
+
+request(url, function (error, response, body) {
+  // code here
+  });
