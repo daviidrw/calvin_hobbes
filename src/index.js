a59cfd7840c1ecf7fb35162b71249c4ababdf125
david <davidw.rq@gmail.com>
2017-11-20 17:09:06 -0300

    add node-crawler package

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 const fs = require('fs')
 const request = require('request')
-const cheerio = require('cheerio')
+const Crawler = require("crawler")
 const url = 'http://www.gocomics.com/calvinandhobbesespanol'
 
-request(url, function (error, response, body) {
-  if (!err && response.statusCode==200){
-    let $ = cheerio.load(body)
-    let img_url = $('picture.img-fluid').children().attr('src')
-    // download the img
-    request(img_url).pipe(fs.createWriteStream('../images/1.jpg'))
+const year = process.env.YEAR || 2017
+const month = process.env.MONTH || 11
+
+let urls = []
+
+function getUrls(){
+  for(i=1;i<31;i++){
+    urls.push(`${url}/${year}/${month}/${i}`)
   }
-  });
+}
+getUrls()
+
+const c = new Crawler({
+  maxConnections : 10,
+  // This will be called for each crawled page
+  callback : (error, res, done) => {
+      if(error){
+          console.log('ERROR')
+          console.log(error)
+      }else{
+          let $ = res.$;
+          let img_url = $('picture.img-fluid').children().attr('src')
+          console.log(img_url)
+          //request(img_url).pipe(fs.createWriteStream(__dirname + `/images/${img_url}.jpg`))
+      }
+      done()
+  }
+});
+c.queue(urls)
+
